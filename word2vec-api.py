3ddc751b7d873df1d440177b04ba5d3d6e2c876c
Vitozhu04 <wenbo_zhu@berkeley.edu>
2022-11-07 18:10:31 +0800

    update for server

--- word2vec-api.py
+++ word2vec-api.py
@@ -13,6 +13,7 @@ from builtins import str
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api, reqparse
 import gensim.models.keyedvectors as word2vec
+from gensim.summarization import keywords
 from gensim import utils, matutils
 from numpy import exp, dot, zeros, outer, random, dtype, get_include, float32 as REAL,\
      uint32, seterr, array, uint8, vstack, argsort, fromstring, sqrt, newaxis, ndarray, empty, sum as np_sum
@@ -20,6 +21,11 @@ import pickle
 import argparse
 import base64
 import sys
+from flask_cors import CORS, cross_origin
+import nltk
+from nltk.tokenize import word_tokenize
+nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt')
 
 parser = reqparse.RequestParser()
 
@@ -33,8 +39,8 @@ def filter_words(words):
 class N_Similarity(Resource):
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('ws1', type=str, required=True, help="Word set 1 cannot be blank!", action='append')
-        parser.add_argument('ws2', type=str, required=True, help="Word set 2 cannot be blank!", action='append')
+        parser.add_argument('ws1', type=str, required=True, help="Word set 1 cannot be blank!", action='append', location=['args'])
+        parser.add_argument('ws2', type=str, required=True, help="Word set 2 cannot be blank!", action='append', location=['args'])
         args = parser.parse_args()
         return model.n_similarity(filter_words(args['ws1']),filter_words(args['ws2'])).item()
 
@@ -42,8 +48,8 @@ class N_Similarity(Resource):
 class Similarity(Resource):
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('w1', type=str, required=True, help="Word 1 cannot be blank!")
-        parser.add_argument('w2', type=str, required=True, help="Word 2 cannot be blank!")
+        parser.add_argument('w1', type=str, required=True, help="Word 1 cannot be blank!", location=['args'])
+        parser.add_argument('w2', type=str, required=True, help="Word 2 cannot be blank!", location=['args'])
         args = parser.parse_args()
         return model.similarity(args['w1'], args['w2']).item()
 
@@ -53,9 +59,9 @@ class MostSimilar(Resource):
         if (norm == "disable"):
             return "most_similar disabled", 400
         parser = reqparse.RequestParser()
-        parser.add_argument('positive', type=str, required=False, help="Positive words.", action='append')
-        parser.add_argument('negative', type=str, required=False, help="Negative words.", action='append')
-        parser.add_argument('topn', type=int, required=False, help="Number of results.")
+        parser.add_argument('positive', type=str, required=False, help="Positive words.", action='append', location=['args'])
+        parser.add_argument('negative', type=str, required=False, help="Negative words.", action='append', location=['args'])
+        parser.add_argument('topn', type=int, required=False, help="Number of results.", location=['args'])
         args = parser.parse_args()
         pos = filter_words(args.get('positive', []))
         neg = filter_words(args.get('negative', []))
@@ -93,8 +99,18 @@ class ModelWordSet(Resource):
         except Exception as e:
             print(e)
             return
+class ExtractPosTags(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('sentence', type=str, required=True, help="sentence cannot be blank!", location=['args'])
+        args = parser.parse_args()
+        #return keywords(args['sentence'])
+        tokenizedWords = word_tokenize(args['sentence'])
+        pos_tags = nltk.pos_tag(tokenizedWords)
+        return pos_tags
 
 app = Flask(__name__)
+cors = CORS(app)
 api = Api(app)
 
 @app.errorhandler(404)
@@ -154,4 +170,5 @@ if __name__ == '__main__':
     api.add_resource(MostSimilar, path+'/most_similar')
     api.add_resource(Model, path+'/model')
     api.add_resource(ModelWordSet, '/word2vec/model_word_set')
-    app.run(host=host, port=port)
+    api.add_resource(ExtractPosTags, '/word2vec/extract_postags')
+    app.run(host=host, port=port, threaded=10)
